f18c9331c28fc4ac75b86a256e69f6418b214e54
Stan <stanimir@sellercloud.com>
2020-01-17 10:45:02 +0200

    Ternary op. refactoring

--- src/SellerCloud.Net.Http/Extensions/StringExtensions.cs
+++ src/SellerCloud.Net.Http/Extensions/StringExtensions.cs
@@ -3,13 +3,8 @@
     public static class StringExtensions
     {
         public static string WithRoute(this string baseUri, string? route)
-        {
-            if (string.IsNullOrWhiteSpace(route))
-            {
-                return baseUri;
-            }
-
-            return $"{baseUri?.TrimEnd('/')}/{route?.TrimStart('/')}";
-        }
+            => string.IsNullOrWhiteSpace(route)
+                ? baseUri
+                : $"{baseUri?.TrimEnd('/')}/{route?.TrimStart('/')}";
     }
 }
