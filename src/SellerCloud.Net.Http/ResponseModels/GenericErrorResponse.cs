5995336aee221fae9361ad24d4b9d35fe9c8e34d
zdravko-g <zdravko.g@sellercloud.com>
2020-02-25 18:30:43 +0200

    Added handling for dot excpetion raised by ASP.NET Core

--- src/SellerCloud.Net.Http/ResponseModels/GenericErrorResponse.cs
+++ src/SellerCloud.Net.Http/ResponseModels/GenericErrorResponse.cs
@@ -8,5 +8,15 @@
 
         public string? ErrorSource { get; set; }
         public string? StackTrace { get; set; }
+
+        public int Status { get; set; }
+        public string? Title { get; set; }
+        public string? TraceId { get; set; }
+        public Errors? Errors { get; set; }
+    }
+
+    internal class Errors
+    {
+        public string[]? OrderId { get; set; }
     }
 }
