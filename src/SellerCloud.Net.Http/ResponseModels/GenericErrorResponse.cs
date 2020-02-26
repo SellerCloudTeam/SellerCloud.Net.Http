97469a73647cf7c2a959125f73f9bbc00b26f689
zdravko-g <zdravko.g@sellercloud.com>
2020-02-26 10:21:38 +0200

    Removed unused properties

--- src/SellerCloud.Net.Http/ResponseModels/GenericErrorResponse.cs
+++ src/SellerCloud.Net.Http/ResponseModels/GenericErrorResponse.cs
@@ -9,14 +9,8 @@
         public string? ErrorSource { get; set; }
         public string? StackTrace { get; set; }
 
-        public int Status { get; set; }
         public string? Title { get; set; }
         public string? TraceId { get; set; }
-        public Errors? Errors { get; set; }
-    }
-
-    internal class Errors
-    {
-        public string[]? OrderId { get; set; }
+        
     }
 }
